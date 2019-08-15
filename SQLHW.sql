b9dcc20d92fce5b92d1bc31ab503ee86cc0ee1f7
AnthonyTurgelis <30847775+AnthonyTurgelis@users.noreply.github.com>
2019-08-15 12:42:22 -0400

    Update SQLHW.sql

--- SQLHW.sql
+++ SQLHW.sql
@@ -75,67 +75,67 @@ ON "Titles" ("title");
 
 #1. List the following details of each employee: employee number, last name, first name, gender, and salary.
 
-SELECT public."Employees".emp_no, public."Employees".last_name, public."Employees".first_name, public."Employees".gender, public."Employees".hire_date, public."Salaries".Salary
-FROM public."Employees"
-INNER JOIN public."Salaries"
-ON public."Employees".emp_no = public."Salaries".emp_no;
+SELECT "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Employees".gender, "Employees".hire_date, "Salaries".Salary
+FROM "Employees"
+INNER JOIN "Salaries"
+ON "Employees".emp_no = "Salaries".emp_no;
 
 2. List employees who were hired in 1986.
 
 SELECT *
-FROM public."Employees"
-WHERE public."Employees".hire_date LIKE '1986%';
+FROM "Employees"
+WHERE "Employees".hire_date LIKE '1986%';
 
 3. List the manager of each department with the following information: department number, department name, the managers employee number, last name, first name, and start and end employment dates.
 
-SELECT public."Employees".emp_no, public."Employees".last_name, public."Employees".first_name, public."Titles".from_date, public."Titles".to_date, public."Dept_Emp".dept_no, public."Dept_Emp".emp_no, public."Departments".dept_no, public."Departments".dept_name
-FROM public."Employees"
-INNER JOIN public."Dept_Emp"
-ON public."Employees".emp_no = public."Dept_Emp".emp_no
-INNER JOIN public."Titles"
-ON public."Employees".emp_no = public."Titles".emp_no
-INNER JOIN public."Departments"
-ON public."Departments".dept_no = public."Dept_Emp".dept_no;
+SELECT "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Titles".from_date, "Titles".to_date, "Dept_Emp".dept_no, "Dept_Emp".emp_no, "Departments".dept_no, "Departments".dept_name
+FROM "Employees"
+INNER JOIN "Dept_Emp"
+ON "Employees".emp_no = "Dept_Emp".emp_no
+INNER JOIN "Titles"
+ON "Employees".emp_no = "Titles".emp_no
+INNER JOIN "Departments"
+ON "Departments".dept_no = "Dept_Emp".dept_no;
 
 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 
-SELECT public."Employees".emp_no, public."Employees".last_name, public."Employees".first_name, public."Dept_Emp".dept_no, public."Dept_Emp".emp_no, public."Departments".dept_no, public."Departments".dept_name
-FROM public."Employees"
-INNER JOIN public."Dept_Emp"
-ON public."Employees".emp_no = public."Dept_Emp".emp_no
-INNER JOIN public."Departments"
-ON public."Departments".dept_no = public."Dept_Emp".dept_no;
+SELECT "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Dept_Emp".dept_no, "Dept_Emp".emp_no, "Departments".dept_no, "Departments".dept_name
+FROM "Employees"
+INNER JOIN "Dept_Emp"
+ON "Employees".emp_no = "Dept_Emp".emp_no
+INNER JOIN "Departments"
+ON "Departments".dept_no = "Dept_Emp".dept_no;
 
 5. List all employees whose first name is "Hercules" and last names begin with "B."
 
 SELECT *
-FROM public."Employees"
-WHERE public."Employees".first_name LIKE 'Hercules' AND public."Employees".last_name LIKE 'B%';
+FROM "Employees"
+WHERE "Employees".first_name LIKE 'Hercules' AND "Employees".last_name LIKE 'B%';
 
 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
-SELECT public."Employees".emp_no, public."Employees".last_name, public."Employees".first_name, public."Departments".dept_name
-FROM public."Employees"
-INNER JOIN public."Dept_Emp"
-ON public."Employees".emp_no = public."Dept_Emp".emp_no
-INNER JOIN public."Departments"
-ON public."Departments".dept_no = public."Dept_Emp".dept_no
-WHERE public."Departments".dept_name LIKE 'Sales';
+SELECT "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Departments".dept_name
+FROM "Employees"
+INNER JOIN "Dept_Emp"
+ON "Employees".emp_no = "Dept_Emp".emp_no
+INNER JOIN "Departments"
+ON "Departments".dept_no = "Dept_Emp".dept_no
+WHERE "Departments".dept_name LIKE 'Sales';
 
 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
-SELECT public."Employees".emp_no, public."Employees".last_name, public."Employees".first_name, public."Departments".dept_name
-FROM public."Employees"
-INNER JOIN public."Dept_Emp"
-ON public."Employees".emp_no = public."Dept_Emp".emp_no
-INNER JOIN public."Departments"
-ON public."Departments".dept_no = public."Dept_Emp".dept_no
-WHERE public."Departments".dept_name LIKE ANY (array['Sales', 'Development']);
+SELECT "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Departments".dept_name
+FROM "Employees"
+INNER JOIN "Dept_Emp"
+ON "Employees".emp_no = "Dept_Emp".emp_no
+INNER JOIN "Departments"
+ON "Departments".dept_no = "Dept_Emp".dept_no
+WHERE "Departments".dept_name LIKE ANY (array['Sales', 'Development']);
 
 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
-SELECT public."Employees".last_name COUNT (public."Employees".last_name)
-FROM public."Employees"
-GROUP BY public."Employees".last_name
+SELECT "Employees".last_name COUNT ("Employees".last_name)
+FROM "Employees"
+GROUP BY "Employees".last_name
 
 #Not sure why this one is not working
